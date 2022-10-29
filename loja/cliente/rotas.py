aca295311d7f8413723759f781141ad7a28a839d
Lucas Aprígio da Silva <78664518+Lucas-Aprigio@users.noreply.github.com>
2022-10-29 16:07:59 -0300

    Update rotas.py

--- loja/cliente/rotas.py
+++ loja/cliente/rotas.py
@@ -13,8 +13,8 @@ import pdfkit
 import stripe
 
 <<<<<<< HEAD
-publishable_key = 'pk_test_51LsvnJD5yIkwXHsgu1JYySHMRU3OrDG9sC0uOi4RZCnfO7Xq68gA0hGthWe49wSMetLkXOmuCK3lsvZSCz8oUxbI0029VolQoA'
-stripe.api_key = 'sk_test_51LsvnJD5yIkwXHsg5x6v4M4Zj04cAUQ2wrWLFBiL8IylsVpbl61hlIeNvEWn9Elg79by9rrBI30U31zA7ZTk1ayW00SUI5zQz3'
+publishable_key = ''
+stripe.api_key = ''
 =======
 publishable_key = ''
 stripe.api_key = ''
